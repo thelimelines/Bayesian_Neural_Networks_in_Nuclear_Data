574b46e6b6cb2e0e31903eb2fdfb2689a789e518
thelimelines <brodierolph1@gmail.com>
2023-10-19 02:08:09 +0100

    Simple Bayesian NN implementation

---
+++ Bayesian NN/BayesianNN_V1_fail.py
@@ -0,0 +1,228 @@
+# Import required libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.callbacks import Callback
+import tensorflow_probability as tfp
+import tensorflow as tf
+import tkinter as tk
+from tkinter import ttk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+import threading
+
+# Function to create posterior
+def posterior_mean_field(kernel_size, bias_size=0, dtype=None):
+    n = kernel_size + bias_size
+    c = np.log(np.expm1(1.))
+    return tf.keras.Sequential([
+        tfp.layers.VariableLayer(2 * n, dtype=dtype),
+        tfp.layers.DistributionLambda(lambda t: tfp.distributions.Independent(
+            tfp.distributions.Normal(loc=t[..., :n],
+                                     scale=1e-5 + tf.nn.softplus(c + t[..., n:])),
+            reinterpreted_batch_ndims=1)),
+    ])
+
+# Function to create prior
+def prior_trainable(kernel_size, bias_size=0, dtype=None):
+    n = kernel_size + bias_size
+    return tf.keras.Sequential([
+        tfp.layers.VariableLayer(n, dtype=dtype),
+        tfp.layers.DistributionLambda(lambda t: tfp.distributions.Independent(
+            tfp.distributions.Normal(loc=t, scale=1),
+            reinterpreted_batch_ndims=1)),
+    ])
+
+# Define custom callback for real-time plotting
+class RealTimePlot(Callback):
+    def __init__(self, ax):
+        self.ax = ax
+
+    def on_train_begin(self, logs={}):
+        self.losses = []
+        self.val_losses = []
+        self.ax.clear()
+
+    def on_epoch_end(self, epoch, logs={}):
+        self.losses.append(logs.get('loss'))
+        self.val_losses.append(logs.get('val_loss'))
+        self.ax.clear()
+        self.ax.plot(self.losses, label='loss')
+        self.ax.plot(self.val_losses, label='val_loss')
+        self.ax.set_xlabel('Epoch')
+        self.ax.set_ylabel('Loss Value')
+        self.ax.legend()
+        self.ax.figure.canvas.draw()
+
+# GUI to allow user to test model on real data
+def predict_binding_energy():
+    neutron_number, proton_number = float(neutron_entry.get()), float(proton_entry.get())
+    input_data = scaler_x.transform(np.array([[neutron_number, proton_number]]))  # scales user input to model inputs
+    
+    # Monte Carlo Dropout: Run prediction multiple times with dropout enabled
+    predictions = np.array([model(input_data, training=True) for _ in range(100)])
+    predictions = predictions.flatten()
+    
+    # Calculate mean and standard deviation
+    mean_prediction = np.mean(predictions)
+    std_dev_prediction = np.std(predictions)
+    
+    # Scale the mean prediction back to real output (keV)
+    mean_prediction = scaler_y.inverse_transform(np.array([[mean_prediction]]))[0][0]
+    
+    # Check if the input data was in the training set
+    in_training_set = any(np.all(X_train == input_data, axis=1))
+    
+    # Retrieve the actual value from the dataset
+    actual_value = df.loc[(df['Neutron Number'] == neutron_number) & (df['Proton Number'] == proton_number)]
+    actual_value = actual_value['Binding Energy per Nucleon (keV)'].values[0] if not actual_value.empty else 'N/A'
+    
+    # Update the result label with the mean prediction and the uncertainty
+    result_label.config(text=f"In Training Set: {in_training_set}\nActual Value: {actual_value}\nPredicted Value: {mean_prediction:.2f} ± {std_dev_prediction:.2f}")
+
+# Function to render the 3D plot with given neutron and proton grids and predicted energies
+def update_plot(neutron_grid, proton_grid, predicted_energy):
+    ax.clear()
+    canvas_3dplot = FigureCanvasTkAgg(fig, master=root)
+    
+    # Plot the neural network prediction in red
+    ax.plot_surface(neutron_grid, proton_grid, predicted_energy.reshape(neutron_grid.shape), 
+               color='r', alpha=0.6)
+    
+    # Plot the real values directly from the dataset in black
+    real_neutrons = df['Neutron Number'].values
+    real_protons = df['Proton Number'].values
+    real_energies = df['Binding Energy per Nucleon (keV)'].values
+    ax.scatter(real_neutrons, real_protons, real_energies, color='black', alpha=0.1)
+    
+    # Align the axes at (0,0,0)
+    ax.set_xlim3d([0, neutron_grid.max()])
+    ax.set_ylim3d([0, proton_grid.max()])
+    ax.set_zlim3d([0, max(predicted_energy.max(), np.nanmax(real_energies))])
+    
+    ax.set_title('NN Prediction vs Real Data')
+    ax.set_xlabel('Neutron Number')
+    ax.set_ylabel('Proton Number')
+    ax.set_zlabel('Binding Energy per Nucleon (keV)')
+    canvas_3dplot.draw()
+
+# Function to prepare the data and call update_plot for rendering
+def update_3D_plot():
+    # Generate a grid of neutron and proton numbers based on the dataset range
+    neutron_numbers = np.linspace(min(df['Neutron Number']), max(df['Neutron Number']), 50)
+    proton_numbers = np.linspace(min(df['Proton Number']), max(df['Proton Number']), 50)
+    neutron_grid, proton_grid = np.meshgrid(neutron_numbers, proton_numbers)
+
+    # Preparing the data for NN prediction
+    grid_data = np.array([neutron_grid.flatten(), proton_grid.flatten()]).T
+    scaled_grid_data = scaler_x.transform(grid_data)
+
+    # Predict binding energy using the trained NN
+    predicted_energy = model.predict(scaled_grid_data)
+    predicted_energy = scaler_y.inverse_transform(predicted_energy).flatten()
+
+    # Call the existing update_plot function
+    update_plot(neutron_grid, proton_grid, predicted_energy)
+
+    # Use draw_idle to reduce jitter
+    canvas_3dplot.draw_idle()
+
+# Load and preprocess data
+df = pd.read_csv('Test NN\\AME2020_converted.csv') # dataframe retrieved from trimmed AME
+
+# X and Y are the inputs and outputs of the model respectively, note X is an array size 2
+X, y = df[['Neutron Number', 'Proton Number']].values, df['Binding Energy per Nucleon (keV)'].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42) #30% testing, 70%training, random_State is a seed set for reproducability
+
+scaler_x, scaler_y = StandardScaler().fit(X_train), StandardScaler().fit(y_train.reshape(-1, 1))
+X_train, X_test = scaler_x.transform(X_train), scaler_x.transform(X_test)
+y_train, y_test = scaler_y.transform(y_train.reshape(-1, 1)).flatten(), scaler_y.transform(y_test.reshape(-1, 1)).flatten()
+
+# GUI setup
+root = tk.Tk()
+root.title('Neural Network Prediction for Nuclear Binding Energy')
+
+neutron_label, neutron_entry = ttk.Label(root, text='Neutron Number:'), ttk.Entry(root)
+neutron_label.grid(row=0, column=0), neutron_entry.grid(row=0, column=1)
+
+proton_label, proton_entry = ttk.Label(root, text='Proton Number:'), ttk.Entry(root)
+proton_label.grid(row=1, column=0), proton_entry.grid(row=1, column=1)
+
+# Function to be run in a separate thread for model training
+def train_model_thread():
+    global model, X_train, y_train, rtplot_ax
+    real_time_plot = RealTimePlot(rtplot_ax)
+    history = model.fit(X_train, y_train, epochs=100, batch_size=32, validation_split=0.2, callbacks=[real_time_plot])
+
+# Function to start the thread
+def start_training_thread():
+    training_thread = threading.Thread(target=train_model_thread)
+    training_thread.start()
+
+train_button = ttk.Button(root, text='Train Model', command=start_training_thread)
+train_button.grid(row=5, columnspan=2)
+
+# Add an "Update Plot" Button
+update_button = ttk.Button(root, text='Update Plot', command=update_3D_plot)
+update_button.grid(row=4, columnspan=2)
+
+predict_button = ttk.Button(root, text='Predict', command=predict_binding_energy)
+predict_button.grid(row=2, columnspan=2)
+
+result_label = ttk.Label(root, text='')
+result_label.grid(row=3, columnspan=2)
+
+# Add Matplotlib Figure for real-time plotting
+rtplot_fig, rtplot_ax = plt.subplots(figsize=(4, 4))
+canvas_rtplot = FigureCanvasTkAgg(rtplot_fig, master=root)
+canvas_rtplot_widget = canvas_rtplot.get_tk_widget()
+canvas_rtplot_widget.grid(row=6, column=0)  # Changed column to 0
+
+# Create a Matplotlib Figure for 3D plotting
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"}, figsize=(4, 4))
+
+# Add Matplotlib Figure for 3D plotting to Tkinter
+canvas_3dplot = FigureCanvasTkAgg(fig, master=root)
+canvas_3dplot_widget = canvas_3dplot.get_tk_widget()
+canvas_3dplot_widget.grid(row=6, column=1)
+
+# Create a frame to hold the toolbar for real-time plotting
+toolbar_frame = tk.Frame(master=root)
+toolbar_frame.grid(row=7, column=0)
+
+# Create a frame to hold the toolbar for 3D plotting
+toolbar_frame_3d = tk.Frame(master=root)
+toolbar_frame_3d.grid(row=7, column=1)
+
+# Add toolbar for the real-time plot inside the frame
+toolbar = NavigationToolbar2Tk(canvas_rtplot, toolbar_frame)
+toolbar.pack()
+toolbar_frame.grid(row=7, column=0)
+
+# Add toolbar for the 3D plot inside the frame
+toolbar_3d = NavigationToolbar2Tk(canvas_3dplot, toolbar_frame_3d)
+toolbar_3d.pack()
+toolbar_frame_3d.grid(row=7, column=1)
+
+# Initialize model but do not train yet
+model = Sequential([
+    tfp.layers.DenseVariational(16, input_dim=2, activation='relu',
+                                make_posterior_fn=posterior_mean_field,
+                                make_prior_fn=prior_trainable),
+    tfp.layers.DenseVariational(16, activation='relu',
+                                make_posterior_fn=posterior_mean_field,
+                                make_prior_fn=prior_trainable),
+    tfp.layers.DenseVariational(8, activation='relu',
+                                make_posterior_fn=posterior_mean_field,
+                                make_prior_fn=prior_trainable),
+    Dropout(0.2),
+    tfp.layers.DenseVariational(1, activation='linear',
+                                make_posterior_fn=posterior_mean_field,
+                                make_prior_fn=prior_trainable)
+])
+model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mae']) 
+
+root.mainloop()
---
+++ Bayesian NN/BayesianNN_V2_simple.py
@@ -0,0 +1,137 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import tensorflow as tf
+import tensorflow_probability as tfp
+import random
+
+# Function to create posterior
+def posterior_mean_field(kernel_size, bias_size=0, dtype=None):
+    n = kernel_size + bias_size
+    c = np.log(np.expm1(1.))
+    return tf.keras.Sequential([
+        tfp.layers.VariableLayer(2 * n, dtype=dtype),
+        tfp.layers.DistributionLambda(lambda t: tfp.distributions.Independent(
+            tfp.distributions.Normal(loc=t[..., :n],
+                                     scale=1e-5 + tf.nn.softplus(c + t[..., n:])),
+            reinterpreted_batch_ndims=1)),
+    ])
+
+# Function to create prior
+def prior_trainable(kernel_size, bias_size=0, dtype=None):
+    n = kernel_size + bias_size
+    return tf.keras.Sequential([
+        tfp.layers.VariableLayer(n, dtype=dtype),
+        tfp.layers.DistributionLambda(lambda t: tfp.distributions.Independent(
+            tfp.distributions.Normal(loc=t, scale=1),
+            reinterpreted_batch_ndims=1)),
+    ])
+
+# Load the dataset
+df = pd.read_csv('Test NN\\AME2020_converted.csv')
+
+# Prepare the data
+X = df[['Neutron Number', 'Proton Number']].values
+y = df['Binding Energy per Nucleon (keV)'].values
+
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Scale the data
+scaler_X = StandardScaler().fit(X_train)
+scaler_y = StandardScaler().fit(y_train.reshape(-1, 1))
+
+X_train_scaled = scaler_X.transform(X_train)
+y_train_scaled = scaler_y.transform(y_train.reshape(-1, 1)).flatten()
+
+# Define the Bayesian Neural Network model
+
+model = tf.keras.Sequential([
+    tfp.layers.DenseVariational(50, input_dim=2, activation='relu',
+                                make_posterior_fn=posterior_mean_field,
+                                make_prior_fn=prior_trainable),
+    tfp.layers.DenseVariational(50, activation='relu',
+                                make_posterior_fn=posterior_mean_field,
+                                make_prior_fn=prior_trainable),
+    tfp.layers.DenseVariational(50, activation='relu',
+                                make_posterior_fn=posterior_mean_field,
+                                make_prior_fn=prior_trainable),
+    tfp.layers.DenseVariational(1, activation='linear',
+                                make_posterior_fn=posterior_mean_field,
+                                make_prior_fn=prior_trainable)
+])
+
+# model = tf.keras.Sequential([
+#     tfp.layers.DenseVariational(20, input_dim=2, activation='relu',
+#                                 make_posterior_fn=posterior_mean_field,
+#                                 make_prior_fn=prior_trainable),
+#     tfp.layers.DenseVariational(20, activation='relu',
+#                                 make_posterior_fn=posterior_mean_field,
+#                                 make_prior_fn=prior_trainable),
+#     tfp.layers.DenseVariational(1, activation='linear',
+#                                 make_posterior_fn=posterior_mean_field,
+#                                 make_prior_fn=prior_trainable)
+# ])
+
+# Compile the model
+model.compile(loss='mean_squared_error', optimizer='adam')
+
+# Train the model
+model.fit(X_train_scaled, y_train_scaled, epochs=200, batch_size=32)
+
+# Predict with uncertainty (Monte Carlo Dropout)
+def predict_with_uncertainty(input_data, n_iter=100):
+    predictions = np.array([model(input_data, training=True) for _ in range(n_iter)])
+    predictions = predictions.squeeze()
+    
+    mean_prediction = np.mean(predictions, axis=0)
+    std_dev_prediction = np.std(predictions, axis=0)
+    
+    return mean_prediction, std_dev_prediction
+
+# Test data scaled
+X_test_scaled = scaler_X.transform(X_test)
+
+# Prediction
+mean_preds, std_preds = predict_with_uncertainty(X_test_scaled)
+
+# Rescale the predictions back to original scale
+mean_preds_rescaled = scaler_y.inverse_transform(mean_preds.reshape(-1, 1)).flatten()
+std_preds_rescaled = std_preds * scaler_y.scale_
+
+# Identify proton and neutron combinations not in the training set
+train_set = set([tuple(x) for x in X_train])
+all_set = set([tuple(x) for x in X])
+diff_set = all_set - train_set
+
+# Randomly select 20 of these combinations
+random_samples = np.array(random.sample(list(diff_set), 50))
+
+# Make predictions using the trained Bayesian Neural Network
+random_samples_scaled = scaler_X.transform(random_samples)
+mean_preds, std_preds = predict_with_uncertainty(random_samples_scaled)
+
+# Rescale the predictions back to original scale
+mean_preds_rescaled = scaler_y.inverse_transform(mean_preds.reshape(-1, 1)).flatten()
+std_preds_rescaled = std_preds * scaler_y.scale_
+
+# Retrieve the actual values and uncertainties from the dataset
+actual_values = []
+actual_uncertainties = []
+for sample in random_samples:
+    actual_row = df.loc[(df['Neutron Number'] == sample[0]) & (df['Proton Number'] == sample[1])]
+    actual_values.append(actual_row['Binding Energy per Nucleon (keV)'].values[0])
+    actual_uncertainties.append(actual_row['Uncertainty (keV)'].values[0])
+
+# Print predictions, uncertainties, and actual values
+for i in range(50):
+    print(f"Neutron Number: {random_samples[i][0]}, Proton Number: {random_samples[i][1]}")
+    print(f"Prediction: {mean_preds_rescaled[i]:.2f} ± {std_preds_rescaled[i]:.2f} keV")
+    
+    # Check if the actual value is within the prediction uncertainty
+    within_uncertainty = mean_preds_rescaled[i] - std_preds_rescaled[i] <= actual_values[i] <= mean_preds_rescaled[i] + std_preds_rescaled[i]
+    symbol = "(:" if within_uncertainty else "X"
+    
+    print(f"Actual Value: {actual_values[i]:.2f} ± {actual_uncertainties[i]:.2f} keV {symbol}")
+    print("---")
